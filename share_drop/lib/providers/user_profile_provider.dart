753a697e5609ff630f368c7367998a050ee145e8
mrscriptword <mrscriptowrd@gmail.com>
2026-05-28 02:20:38 +0700

    feat: implement UserProfileNotifier and userProfileProvider for device state management

--- share_drop/lib/providers/user_profile_provider.dart
+++ share_drop/lib/providers/user_profile_provider.dart
@@ -1,10 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/user_profile.dart';
 
-final userProfileProvider = Provider<UserProfile>((ref) {
-  return UserProfile(
-    deviceId: 'pubel-1234',
-    deviceName: 'Pubel Preview',
-    deviceSub: 'Demo Mode',
+class UserProfileNotifier extends StateNotifier<UserProfile> {
+  UserProfileNotifier() : super(
+    UserProfile(
+      deviceId: 'pubel-1234',
+      deviceName: 'Pubel Preview',
+      deviceSub: 'Demo Mode',
+    )
   );
+
+  void updateDeviceName(String newName) {
+    state = UserProfile(
+      deviceId: state.deviceId,
+      deviceName: newName,
+      deviceSub: state.deviceSub,
+    );
+  }
+}
+
+final userProfileProvider = StateNotifierProvider<UserProfileNotifier, UserProfile>((ref) {
+  return UserProfileNotifier();
 });
